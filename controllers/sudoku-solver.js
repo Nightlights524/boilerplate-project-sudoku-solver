0679145e29cb03b541e52de05662af7d98ddcbb4
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-30 11:56:57 -0600

    Add (very) naive checkRegionPlacement() function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -50,7 +50,33 @@ class SudokuSolver {
   // }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    // hard coded regions by index - ughhhhh
+    const regions = [
+      [0, 1, 2, 9, 10, 11, 18, 19, 20],
+      [3, 4, 5, 12, 13, 14, 21, 22, 23],
+      [6, 7, 8, 15, 16, 17, 24, 25, 26],
+      [27, 28, 29, 36, 37, 38, 45, 46, 47],
+      [30, 31, 32, 39, 40, 41, 48, 49, 50],
+      [33, 34, 35, 42, 43, 44, 51, 52, 53],
+      [54, 55, 56, 63, 64, 65, 72, 73, 74],
+      [57, 58, 59, 66, 67, 68, 75, 76, 77],
+      [60, 61, 62, 69, 70, 71, 78, 79, 80],
+    ];
 
+    const rows = ['A','B','C','D','E','F','G','H','I'];
+    const startingIndex = rows.indexOf(row.toUpperCase()) * 9;
+    const indexOfCoord = startingIndex + column - 1;
+
+    for (const region of regions) {
+      if (region.includes(indexOfCoord)) {
+        for (const index of region) {
+          if (puzzleString[index] === value) {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
   }
 
   solve(puzzleString) {
