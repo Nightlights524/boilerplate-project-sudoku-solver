71b5995e72fc564e5c2595cd3c5e2ba017edb089
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-30 11:21:43 -0600

    Add checkRowPlacement() function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -11,15 +11,17 @@ class SudokuSolver {
   }
   
   // ROWS ARE A-I, COLUMNS are 1-9
-  // checkRowPlacement(puzzleString, row, column, value) {
-  //   const startingIndex = column - 1;
-  //   for (let index = startingIndex; index < startingIndex + 9; ++index) {
-  //     if (puzzleString[index] === value) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // }
+  checkRowPlacement(puzzleString, row, column, value) {
+    const rows = ['A','B','C','D','E','F','G','H','I'];
+    const startingIndex = rows.indexOf(row.toUpperCase()) * 9;
+
+    for (let index = startingIndex; index < startingIndex + 9; ++index) {
+      if (puzzleString[index] === value) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   // checkRowPlacement2(puzzleString, row, column, value) {
   //   for (let i = 0; i < 9; i++) {
