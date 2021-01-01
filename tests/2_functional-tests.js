f0836102ec9a6215934afcbc5fd49ff5b1e8bfe1
Nightlights524 <Nightlights524@users.noreply.github.com>
2020-12-31 22:57:46 -0600

    Skeleton code for Functional Tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,5 +7,145 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
 
+  test('Solve a puzzle with valid puzzle string', done => {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Solve a puzzle with missing puzzle string', done => {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Solve a puzzle with invalid characters', done => {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Solve a puzzle with incorrect length', done => {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Solve a puzzle that cannot be solved', done => {
+    chai.request(server)
+      .post('/api/solve')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with all fields', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with single placement conflict', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with multiple placement conflicts', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with all placement conflicts', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with missing required fields', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with invalid characters', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with incorrect length', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with invalid placement coordinate', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
+  test('Check a puzzle placement with invalid placement value', done => {
+    chai.request(server)
+      .post('/api/check')
+      .send({})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
+  });
+
 });
 
